0a7f51e86fd657d4ea5d0e5352596ff14e378ae5
ajith82 <ajith@sketchbrahma.com>
2023-10-31 20:52:29 +0530

    db connected,model file created

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Node API app is running on port 3000`);
-})
+mongoose
+  .connect(
+    "mongodb+srv://ajithvc45:Ajithvc45@cluster0.sq4kclp.mongodb.net/todo?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`Node API app is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
